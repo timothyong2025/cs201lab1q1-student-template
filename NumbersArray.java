ed27bee4b368a3440c33925b0b50cb85ccf74e6d
timothyong2025 <timothy.ong.2025@computing.smu.edu.sg>
2026-09-02 23:04:11 +0800

    first_attempt

--- NumbersArray.java
+++ NumbersArray.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+
 public class NumbersArray {
     public static void main(String[] args) {
         Integer[] input = {1,2,3,4,1,5};
@@ -20,6 +21,81 @@ public class NumbersArray {
     }
 
     // Write your methods here
-    
-}
+    public static int findMax(Integer[] input){
+        //if array is not sorted
+        if(input == null || input.length == 0){
+            return 0;
+        }
+        int biggest = input[0];
+        for (int num : input){
+            if(num > biggest){
+                biggest = num;
+            }
+        }
+        return biggest;
+    }
 
+
+    public static int[] findDuplicates(Integer[] input){
+        //if invalid input
+        if(input == null || input.length == 0){
+            return null;
+        }
+
+        //track current array
+        ArrayList<Integer> seen = new ArrayList<>();
+        ArrayList<Integer> output = new ArrayList<>();
+
+        //look through array for each element and check if there's a dupe
+        for (int num : input){
+            if(seen.contains(num)){
+                output.add(num);
+            }
+            if(!seen.contains(num)){
+                seen.add(num);
+            }
+        }
+
+        int[] output2 = output.stream().mapToInt(Integer::intValue).toArray();
+        return output2;
+
+    }
+
+    public static int[] findUnique(Integer[] input){
+        //if invalid input
+        if(input == null || input.length == 0){
+            return null;
+        }
+
+        //track current array
+        ArrayList<Integer> seen = new ArrayList<>();
+        ArrayList<Integer> dupe = new ArrayList<>();
+        ArrayList<Integer> output = new ArrayList<>();
+
+        //look through array for each element and check if there's a dupe
+        for (int num : input){
+            if(seen.contains(num)){
+                dupe.add(num);
+            }
+            if(!seen.contains(num)){
+                seen.add(num);
+            }
+        }
+        
+        for (int num : input){
+            if(!dupe.contains(num)){
+                output.add(num);
+            }
+            
+        }
+
+
+
+        int[] output2 = output.stream().mapToInt(Integer::intValue).toArray();
+        return output2;
+
+    }
+
+
+}
+    
